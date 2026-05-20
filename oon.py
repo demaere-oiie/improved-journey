ba16bd29bddb33dddcae8ba91f78b2c7457a7839
Da Def <da.def.lang.belta@gmail.com>
2026-05-20 17:19:32 +0200

    diffs

--- oon.py
+++ oon.py
@@ -2,6 +2,7 @@ import numpy as np
 from keras.preprocessing import image
 import cv2 as cv
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
 from skimage.io import imread
 from pathlib import Path
 print("dependencies loaded")
@@ -9,31 +10,52 @@ print("dependencies loaded")
 train = []
 p = Path("../../raw/rc")
 for file in p.iterdir():
-    print(file)
     img = imread(file)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.resize(img, (32, 32), interpolation=cv.INTER_AREA)
     train.append(img)
 p = Path("../../raw/oct")
 for file in p.iterdir():
-    print(file)
     img = imread(file)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.resize(img, (32, 32), interpolation=cv.INTER_AREA)
     train.append(img)
 
-plt.figure(figsize=(10, 10))
-for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(np.array(train[i]).astype("uint8"))
-        plt.axis("off")
+if 0:
+  plt.figure(figsize=(10, 10))
+  for i in range(9):
+          ax = plt.subplot(3, 3, i + 1)
+          plt.imshow(np.array(train[i]).astype("uint8"))
+          plt.axis("off")
 
-plt.show()
+  plt.show()
 
-plt.figure(figsize=(10, 10))
-for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(np.array(train[-(i+1)]).astype("uint8"))
-        plt.axis("off")
+  plt.figure(figsize=(10, 10))
+  for i in range(9):
+          ax = plt.subplot(3, 3, i + 1)
+          plt.imshow(np.array(train[-(i+1)]).astype("uint8"))
+          plt.axis("off")
 
-plt.show()
+  plt.show()
+
+X_train = np.array(train)
+X_train = np.reshape(X_train, (48, 32*32))
+print(X_train.shape)
+y0 = np.zeros(24)
+y1 = np.ones(24)
+y_train = np.concatenate((y0,y1), axis=0)
+
+knn = KNeighborsClassifier(n_neighbors=4)
+
+print(knn.fit(X_train, y_train))
+
+y_pred = knn.predict(X_train)
+
+for i,t in enumerate(y_train):
+    if t != y_pred[i]:
+        plt.figure(figsize=(10, 10))
+        for j in range(1):
+            ax = plt.subplot(1, 1, j+1)
+            plt.imshow(np.array(train[i]).astype("uint8"))
+            plt.axis("off")
+        plt.show()
