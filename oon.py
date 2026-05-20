879a893abf100dc09d3c489d39070dfac81111b2
Da Def <da.def.lang.belta@gmail.com>
2026-05-20 16:05:47 +0200

    sample

--- oon.py
+++ oon.py
@@ -1,5 +1,7 @@
+import numpy as np
 from keras.preprocessing import image
 import cv2 as cv
+import matplotlib.pyplot as plt
 from skimage.io import imread
 from pathlib import Path
 print("dependencies loaded")
@@ -11,7 +13,6 @@ for file in p.iterdir():
     img = imread(file)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.resize(img, (32, 32), interpolation=cv.INTER_AREA)
-    img = img / 255.
     train.append(img)
 p = Path("../../raw/oct")
 for file in p.iterdir():
@@ -19,5 +20,20 @@ for file in p.iterdir():
     img = imread(file)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.resize(img, (32, 32), interpolation=cv.INTER_AREA)
-    img = img / 255.
     train.append(img)
+
+plt.figure(figsize=(10, 10))
+for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(np.array(train[i]).astype("uint8"))
+        plt.axis("off")
+
+plt.show()
+
+plt.figure(figsize=(10, 10))
+for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(np.array(train[-(i+1)]).astype("uint8"))
+        plt.axis("off")
+
+plt.show()
